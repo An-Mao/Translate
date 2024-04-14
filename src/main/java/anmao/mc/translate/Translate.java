1f8ab5c5006b5ee21bf75e83a5792f1fc6e053c0
AnMao <lxxl@live.cn>
2024-04-14 17:46:30 +0800

    +

--- src/main/java/anmao/mc/translate/Translate.java
+++ src/main/java/anmao/mc/translate/Translate.java
@@ -14,22 +14,6 @@ public class Translate
     public static final String MOD_ID = "translate";
     public Translate() {
         System.out.println("test :: "+ Component.literal("test 1"));
-
-        /*
-        Object result = EasyJS.creat()
-                .addParameter("source","test")
-                .runFile(Configs.ConfigDir_JS+"baidu.js");
-
-        String re =  Network.sendData("https://fanyi-api.baidu.com/api/trans/vip/translate","POST","Content-Type:application/x-www-form-urlencoded;",result.toString());
-        System.out.println("result2::"+URLDecoder.decode("\\u6d4b\\u9a8c", StandardCharsets.UTF_8));
-        System.out.println("result::"+re);
-        result = EasyJS.creat()
-                .addParameter("translate",re)
-                .runFile(Configs.ConfigDir_JS+"baiduR.js");
-        System.out.println("result5::"+result);
-        System.exit(0);
-
-         */
     }
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
